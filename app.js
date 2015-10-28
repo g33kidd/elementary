4f848cf0e898e7a2b487ce1dce90b1fdec5f33a2
Joshua Kidd <kidd.josh.343@gmail.com>
2015-10-27 21:22:35 -0600

    Added basic request handling

--- app.js
+++ app.js
@@ -10,53 +10,17 @@ var router = new Router();
 // Only provide the route and callback, nothing else.
 // router.get('/test/:postid', () => console.log("test"));
 
-// provide params, route, callback, and other stuff by merging params?
-
-
-// router.route('routeName', {
-//   path: '/',
-//   action(request, response) {
-//     console.log(request.url);
-//     response.end();
-//   }
-// });
-
-
-// Should result in an error?
-// router.route('routeName', {
-//   path: '/',
-//   action(request, response) {
-//     console.log(request.url);
-//     response.end();
-//   }
-// });
-
 router.route('home', {
   path: '/',
   action(request, response) {
-    console.log(request.url);
     response.end();
   }
 });
 
-console.log(router.listRoutes());
+var RequestListener = (req, res) => {
+  router.requestHandler(req, res);
+};
 
-// Maybe good idea to have different request type methods as well
-// router.route('routeName', {
-//   path: '/',
-//
-//   actionGet(response) {
-//     response.end();
-//   },
-//   actionPost(response) {
-//     response.end();
-//   },
-//   actionDelete(response) {
-//     response.end();
-//   }
-// });
-
-var RequestListener = (req, res) => {};
 http.createServer(RequestListener).listen(3000, () => {
   console.log("[[[ "+ __dirname +" ]]]\n\nNow listening on: http://localhost:3000");
 });
--- core/router/router.js
+++ core/router/router.js
@@ -1,6 +1,27 @@
 var _ = require('underscore');
 import { Route } from './route.js'
 
+// Usage:
+// var router = new Router();
+//
+// router.route('home', {
+//   path: '/',
+//   action(request, response) {
+//     console.log(request.url);
+//     response.end();
+//   }
+// });
+//
+// TODO: Add shorthand route method
+// router.route('routeName', 'routePath', callback)
+//
+// TODO: Add request types for action methods
+// actionGet or action  : GET Request
+// actionPost           : POST Request
+// actionDelete         : DELETE Request
+//
+// TODO: Improve comments and Documentation
+
 export class Router {
   constructor() {
     this.routes = [];
@@ -8,7 +29,7 @@ export class Router {
 
   route(name, options) {
     let newRoute = new Route(name, options.path, options.action);
-    if(this._validateRoute(newRoute)) {
+    if(this.validateRoute(newRoute)) {
       this.routes.push(newRoute);
     }else{
       throw new Error(`Route is Invalid`);
@@ -18,24 +39,20 @@ export class Router {
 
   listRoutes() { return this.routes; }
 
-  _validateRoute(route) {
-    // TODO: improve validation
+  validateRoute(route) {
     // TODO: make sure that routes dont override core routes. reserved words.
-
     if(this.findRoute(route.name) || this.findRoute(route.path)) {
       return false;
     }else{
       return true;
     }
-    // check if route already exists
-    //  by name, by pattern, and action
-    //  if name and patter exist for each request type, its fine I guess
-    // validate pattern
-    // validate param format
-    // validate other things
   }
 
-  // should be able to find a route based on its name
+  /**
+   * Checks if a route already exists based on name or path as a param.
+   * @param  {String} param   name or url/path of the route your looking for
+   * @return {Bool}
+   */
   findRoute(param) {
     return _.find(this.routes, (route) => {
       if(route.name === param || route.path === param) {
@@ -46,5 +63,17 @@ export class Router {
         return false;
       }
     });
+  }
+
+  getRoute(path) {
+    if(this.findRoute(path)) {
+      return _.findWhere(this.routes, { path: path });
+    }
+  }
+
+  requestHandler(req, res) {
+    let route = this.getRoute(req.url);
+    route.handleRequest(req, res);
+    console.log(route);
   }
 }
